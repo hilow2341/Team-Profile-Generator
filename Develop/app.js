6edfbe54c31c7746d5baa773cf5f08bb89ea8e96
hilow2341 <60238749+hilow2341@users.noreply.github.com>
2020-07-09 18:09:33 -0400

    Added switch cases and functionality to write an html file.

--- Develop/app.js
+++ Develop/app.js
@@ -78,3 +78,88 @@ async function start(){
             title = data.title;
             email = data.email;
         });
+
+        // Switch Case depending on the title of the employee
+        switch (title){
+            case "Manager":
+
+                // ask user of Manager's Office Number
+                await inquirer.prompt([
+                    {
+                        type: "input",
+                        message: "What is your Manager's Office Number?",
+                        name: "officeNo"
+                    }
+                ])
+                .then((data) => {
+
+                    // Create a new object with all avaiable user input data
+                    const manager = new Manager(name, id, email, data.officeNo);
+
+                    // Reads and places HTML from manager.html in teamMemever Variable
+                    teamMember = fs.readFileSync("templates/manager.html");
+
+                    // Uses eval() to pass template literals from html files.
+                    // Adds the string to the team HTML.
+                    teamHTML = teamHTML + "\n" + eval('`'+ teamMember +'`');
+                });
+                break;
+
+            //Steps Similar to Manager but for intern
+            case "Intern":
+                await inquirer.prompt([
+                    {
+                        type: "input",
+                        message: "What school is your Intern attending?",
+                        name: "school"
+                    }
+                ])
+                .then((data) => {
+                    const intern = new Intern(name, id, email, data.school);
+                    teamMember = fs.readFileSync("templates/intern.html");
+                    teamHTML = teamHTML + "\n" + eval('`'+ teamMember +'`');
+                });
+                break;
+
+            //Steps Similar to Manager but for engineer
+            case "Engineer":
+                await inquirer.prompt([
+                    {
+                        type: "input",
+                        message: "What is your Engineer's GitHub?",
+                        name: "github"
+                    }
+                ])
+                .then((data) => {
+                    const engineer = new Engineer(name, id, email, data.github);
+                    teamMember = fs.readFileSync("templates/engineer.html");
+                    teamHTML = teamHTML + "\n" + eval('`'+ teamMember +'`');
+                });
+                break;
+
+        } // End of Switch Case
+
+    } // End of For loop
+
+    // Reads main.html and places html in a variable
+    const mainHTML = fs.readFileSync("templates/main.html");
+    
+    // Use eval to implement template literals in main.html and places teamHTML inside main template
+    teamHTML = eval('`'+ mainHTML +'`');
+
+    // write file to new team.html file
+    fs.writeFile("output/team.html", teamHTML, function(err) {
+
+        if (err) {
+          return console.log(err);
+        }
+      
+        console.log("Success!");
+      
+      });
+
+    // console.log(teamHTML);
+}
+
+
+start();
