15cccd286f18d636d383c0b1652709aeb4d06913
hilow2341 <60238749+hilow2341@users.noreply.github.com>
2020-07-09 17:49:22 -0400

    Added employee input functionality to index.js

--- Develop/app.js
+++ Develop/app.js
@@ -5,3 +5,76 @@ const fs = require("fs");
 const Manager = require("./lib/Manager");
 const Intern = require("./lib/Intern");
 const Engineer = require("./lib/Engineer");
+
+// ===== START FUNCTION ===== 
+async function start(){
+    console.log("Let's make your Dream Team!");
+
+    // Set Variable to hold HTML
+    let teamHTML = "";
+
+    // Variable to hold number of team members
+    let teamSize;
+
+    // First Question to ask to set up loop
+    await inquirer.prompt(
+        {
+            type: "number",
+            message: "How many people are in your team?",
+            name: "noOfTeamMem"
+        }
+    )
+    .then((data) => {
+
+        // Number of team members placed in teamSize for scope purposes.
+        // 1 is added start from 1 rather than 0 for user understanding.
+        teamSize = data.noOfTeamMem + 1;
+    });
+    
+    // If Team Size is 0, will end program
+    if (teamSize === 0){
+        console.log("I guess there is no one on your team...");
+        return;
+    }
+    
+    // Loop begins to ask questions depending on the size of the team
+    for(i = 1; i < teamSize; i++){
+
+        // Global variables set
+        let name;
+        let id;
+        let title;
+        let email;
+
+        // Prompts user to answer the basic questions of the employee
+        await inquirer.prompt([ 
+            {
+                type: "input",
+                message: `What is employee (${i})'s name?`,
+                name: "name"
+            },
+            {
+                type: "input",
+                message: `What is the employee (${i})'s id?`,
+                name: "id"
+            },
+            {
+                type: "input",
+                message: `What is the employee (${i})'s Email?`,
+                name: "email"
+            },
+            {
+                type: "list",
+                message: `what the employee (${i})'s title?`,
+                name: "title",
+                choices: ["Engineer", "Intern", "Manager"]
+            }
+        ])
+        .then((data) => {
+
+            // Takes data from user and places value in global variables
+            name = data.name;
+            id = data.id;
+            title = data.title;
+            email = data.email;
+        });
